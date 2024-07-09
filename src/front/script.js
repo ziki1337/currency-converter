0c39a7bcc4c1c9bc2d11fdd28d6cbbcd9a5505fe
ziki1337 <artem_belokurov@inbox.ru>
2024-07-09 15:50:36 +0300

    Обновил фронт под калькулятор

--- src/front/script.js
+++ src/front/script.js
@@ -42,3 +42,40 @@ document.getElementById('convertButton').addEventListener('click', async () => {
         document.getElementById('result').innerText = 'Error converting currency';
     }
 });
+
+const fromSelect = document.getElementById('from-currency');
+const toSelect = document.getElementById('to-currency');
+const currency1Select = document.getElementById('currency1');
+const currency2Select = document.getElementById('currency2');
+const finalCurrencySelect = document.getElementById('final-currency');
+const resultContainer = document.getElementById('results-container')
+
+document.getElementById('calculate-btn').addEventListener('click', async () => {
+
+    const amount1 = document.getElementById('amount1').value;
+    const currency1 = currency1Select.value;
+    const amount2 = document.getElementById('amount2').value;
+    const currency2 = currency2Select.value;
+    const finalCurrency = finalCurrencySelect.value;
+    const operation = document.getElementById('operation').value;
+
+    try {
+        const response = await fetch('http://localhost:2727/currency-converter/calculator', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ num1: amount1, firstCurr: currency1, num2: amount2, secondCurr: currency2, finalCurr: finalCurrency, operation }),
+        });
+
+        const data = await response.json();
+        const resultString = `${amount1} ${currency1} ${operation} ${amount2} ${currency2} = ${data.convertedAmount.toFixed(2)} ${finalCurrency}`;
+
+        const resultElement = document.createElement('div');
+        resultElement.textContent = resultString;
+        resultContainer.appendChild(resultElement);
+        
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
